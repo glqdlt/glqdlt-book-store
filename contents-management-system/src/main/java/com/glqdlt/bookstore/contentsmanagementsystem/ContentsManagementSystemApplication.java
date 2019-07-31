a59d6386d3cd3158ab42e66bb1608310bc86272b
glqdlt <dlfdnd0725@gmail.com>
2019-07-31 19:46:38 +0900

    CMS 추가

---
+++ contents-management-system/src/main/java/com/glqdlt/bookstore/contentsmanagementsystem/ContentsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.bookstore.contentsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ contents-management-system/src/test/java/com/glqdlt/bookstore/contentsmanagementsystem/ContentsManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glqdlt.bookstore.contentsmanagementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContentsManagementSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
