a9b98e7cba2756e6cdaa91f29c3c74a9ae9086c2
glqdlt <dlfdnd0725@gmail.com>
2019-07-31 19:19:54 +0900

    Api 서버 추가

---
+++ bookstroe-api-server/src/main/java/com/glqdlt/bookstroe/bookstroeapiserver/BookstroeApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.bookstroe.bookstroeapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstroeApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstroeApiServerApplication.class, args);
+    }
+
+}
---
+++ bookstroe-api-server/src/test/java/com/glqdlt/bookstroe/bookstroeapiserver/BookstroeApiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glqdlt.bookstroe.bookstroeapiserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstroeApiServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
